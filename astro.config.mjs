36395d89d2c6e11f281fe37a29bfef2afea6149c
skyeto <mail@skyeto.com>
2023-12-13 14:10:07 +0100

    fixed dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
@@ -13,6 +11,8 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 import react from "@astrojs/react";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
