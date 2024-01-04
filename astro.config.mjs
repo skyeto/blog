0b64e0c9bd6d2eee8e270d8684af9f9f0abf1dcd
skyeto <mail@skyeto.com>
2024-01-04 13:25:32 +0100

    Updated design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
-import compress from "astro-compress";
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -15,7 +10,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://skyeto.com",
-  integrations: [tailwind(), mdx(), prefetch(), compress(), react()]
+  integrations: [tailwind(), mdx(), /*prefetch(),*/ react()]
 });
